5839678a6e26e789d42ffb9692bbb70c6ab0b750
Udayasuryan Kodoly <udayasurya007@gmail.com>
2018-02-19 12:04:36 -0800

    Inital commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now =  new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next)=>{
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance Mode',
+//     warningMessage: 'This site is under maintenance. please visit after one hour'
+//   });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+   return new Date().getFullYear()
+});
+
+hbs.registerHelper('ScremIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res)=> {
+//   res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage:'Welcome to home page',
+  });
+});
+
+app.get('/about', (req, res) => {
+ res.render('about.hbs', {
+   pageTitle: 'About Page',
+ });
+});
+
+app.get('/bad',(req, res) => {
+  res.send({
+    ErrorMessage: 'Unable to handle request'
+  });
+});
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
